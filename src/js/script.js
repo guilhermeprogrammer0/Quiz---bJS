c6349672ffa68da1394af5f57aba3a2296078c71
Guilherme Souza <guilherme003souza@gmail.com>
2025-02-05 12:21:09 -0300

    Mudando

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+import {questoes} from './banco-questoes.js';
 const inicioQuiz = document.querySelector(".inicioQuiz");   
 const perguntas = document.querySelector(".perguntas");
 const respostas = document.querySelector(".respostas");
@@ -67,45 +68,3 @@ function finalizarQuiz(){
     `
 }
 
-const questoes = [
-    {
-        questao: "Qual é a área de um quadrado que tem lado de 6 cm?",
-        resposta:[
-            
-                {texto: "36 cm quadrados", correta: true},
-                {texto: "9 cm quadrados", correta: false},
-                {texto: "8 m quadrados", correta: false},
-                {texto: "10 cm quadrados", correta: false}
-        ]
-    },
-    {
-        questao: "Qual é a soma de 25 e 37?",
-        resposta:[
-            
-                {texto: "85", correta: false},
-                {texto: "62", correta: true},
-                {texto: "15", correta: false},
-                {texto: "2", correta: false}
-        ]
-    },
-    {
-        questao: "20% de 100",
-        resposta:[
-            
-                {texto: "50", correta: false},
-                {texto: "89", correta: false},
-                {texto: "4", correta: false},
-                {texto: "20", correta: true}
-        ]
-    },
-    {
-        questao: "Qual é a média dos números 10, 15 e 20?",
-        resposta:[
-            
-                {texto: "52", correta: false},
-                {texto: "10", correta: false},
-                {texto: "5", correta: false},
-                {texto: "Nenhuma das alternativas", correta: true}
-        ]
-    }
-]
